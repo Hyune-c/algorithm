1f858b09c36eeb1da32367e217322d4b1f2b4803
Byung Hyun Choi <choi8608@gmail.com>
2020-06-20 17:00:33 +0900

    Q2557 Hello World

---
+++ src/baekjoon/q2557/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.q2557;
+
+class Solution {
+
+  public void solution() {
+    System.out.println("Hello World!");
+  }
+}
+
+public class Main {
+
+  public static void main(String[] args) {
+    new Solution().solution();
+  }
+}
